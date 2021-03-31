e211ef5d384a95ec26ba47fcc8f9d6621aca7514
gputnam <gputnam@sbndbuild01.fnal.gov>
2021-03-31 14:17:57 -0500

    Initial commit w/ setup scripts.

---
+++ setup_icarus.sh
@@ -0,0 +1,3 @@
+source /cvmfs/icarus.opensciencegrid.org/products/icarus/setup_icarus.sh
+setup sam_web_client v3_0
+setup python v3_8_3b
---
+++ setup_sbnd.sh
@@ -0,0 +1,3 @@
+source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh
+setup sam_web_client v3_0
+setup python v3_8_3b
