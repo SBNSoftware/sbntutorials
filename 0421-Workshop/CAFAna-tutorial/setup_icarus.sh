8f792b228d0897fc31fdd543d47392b052ee7256
Edward Tyley <e.tyley@sheffield.ac.uk>
2021-04-05 04:21:40 -0500

    Update setup scripts to latest versions of sbnana

--- 0421-Workshop/CAFAna-tutorial/setup_icarus.sh
+++ 0421-Workshop/CAFAna-tutorial/setup_icarus.sh
@@ -1,2 +1,2 @@
 source /cvmfs/icarus.opensciencegrid.org/products/icarus/setup_icarus.sh
-setup sbnana v09_19_00 -q e19:prof
+setup sbnana v09_19_00_02 -q e19:prof
--- 0421-Workshop/CAFAna-tutorial/setup_sbnd.sh
+++ 0421-Workshop/CAFAna-tutorial/setup_sbnd.sh
@@ -1,2 +1,2 @@
 source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh
-setup sbnana v09_19_00 -q e19:prof
+setup sbnana v09_19_00_02 -q e19:prof
